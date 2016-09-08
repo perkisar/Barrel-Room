a8aa4a0a8778b4385b65051066565b5ca3659667
Sarah Perkins <sarah@vancouverbolt.com>
2016-09-08 09:46:44 -0700

    Initial Commit

---
+++ js/menu.js
@@ -0,0 +1,31 @@
+﻿function populateMenu() {
+    var container = $('.tab-content');
+
+    // if lunch tab (default)
+
+    // if dinner tab
+
+    // if happy hour tab
+
+    // if wine tab
+
+    // if cocktail tab
+};
+
+
+function menuToggle(id) {
+
+    console.log(id.childNodes);
+
+    p = $(id.childNodes[3]);
+    tab = $(id.childNodes[5]);
+    if (p.hasClass("hidden")) {
+        p.removeClass("hidden");
+        tab.removeClass("hidden");
+    }
+    else {
+        p.addClass("hidden");
+        tab.addClass("hidden");
+    }
+
+};
---
+++ js/screenSize.js
@@ -0,0 +1,17 @@
+﻿//function adjustStyle(width) {
+//    width = parseInt(width);
+//    if (width < 601) {
+//        $("#size-stylesheet").attr("href", "../css/accordian-small.css");
+//    //} else if (width < 900) {
+//    //    $("#size-stylesheet").attr("href", "css/medium.css");
+//    } else {
+//        $("#size-stylesheet").attr("href", "../css/accordian-large.css");
+//    }
+//}
+
+//$(function () {
+//    adjustStyle($(this).width());
+//    $(window).resize(function () {
+//        adjustStyle($(this).width());
+//    });
+//});
